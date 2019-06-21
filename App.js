2836a22c9638c40d996c8f125a5fa48b1abc45e6
YeisonGomez <yeigomez97@hotmail.com>
2019-06-21 01:03:52 -0500

    install native base

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 import Home from './src/scenes/Home/Home';
+import Login from './src/scenes/Login/Login';
+import Signup from './src/scenes/Signup/Signup';
  
 const MainNavigator = createStackNavigator({
-  Home: {screen: Home} 
+  Signup: {screen: Signup},
+  Home: {screen: Home},
+  Login: {screen: Login},
 });
 
 const App = createAppContainer(MainNavigator);
--- src/scenes/Signup/Signup.js
+++ src/scenes/Signup/Signup.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import BackgroundGeolocation from "react-native-background-geolocation";
-
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Container, Header, Content, Button, Text } from 'native-base';
+ 
+import { Platform, StyleSheet, View } from 'react-native';   
 
 export default class Signup extends Component {
 
@@ -15,9 +15,18 @@ export default class Signup extends Component {
     const {  } = this.state;
 
     return (
-      <View style={styles.container}>
-        
-      </View>
+      <Container>
+      <Header />
+      <Content>
+        <Button light><Text> Light </Text></Button>
+        <Button primary><Text> Primary </Text></Button>
+        <Button success><Text> Success </Text></Button>
+        <Button info><Text> Info </Text></Button>
+        <Button warning><Text> Warning </Text></Button>
+        <Button danger><Text> Danger </Text></Button>
+        <Button dark><Text> Dark </Text></Button>
+      </Content>
+    </Container>
     );
   }
 }
